7774bbe2c203a125581b897f35dfb41dd93887b2
Kelvin Rimui <rimuikelvin4@gmail.com>
2024-06-24 15:19:51 +0300

    Initial commit

---
+++ src/components/header/header.data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: 'home',
+    label: 'Home',
+  },
+  {
+    path: 'feature',
+    label: 'About',
+  },
+  {
+    path: 'pricing',
+    label: 'How It Works',
+  },
+  {
+    path: 'testimonial',
+    label: 'Testimonial',
+  },
+];
