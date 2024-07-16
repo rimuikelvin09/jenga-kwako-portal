5b68dd0cc671c828a188df106020a7ab4cb4fe1a
Kelvin Rimui <rimuikelvin4@gmail.com>
2024-07-16 12:13:09 +0300

    Added the hubspot form to the sign-up button

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,9 +7,13 @@ import LogoDark from 'assets/Jengalogo.png';
 import { DrawerProvider } from '../../contexts/drawer/drawer.provider';
 import MobileDrawer from './mobile-drawer';
 import menuItems from './header.data';
+import useHubSpotForm from './useHubSpotForm';
 
 export default function Header({ className }) {
+  const { loadForm, closeForm, isFormVisible } = useHubSpotForm('139636134', 'e3dbe126-3d3c-4f80-bc4f-3cabb8a9381e');
+
   return (
+
     <DrawerProvider>
       <header sx={styles.header} className={className} id="header">
         <Container sx={styles.container}>
@@ -35,6 +39,7 @@ export default function Header({ className }) {
             className="donate__btn"
             variant="secondary"
             aria-label="Sign-Up"
+            onClick={loadForm}
           >
             Sign-Up
           </Button>
@@ -42,7 +47,18 @@ export default function Header({ className }) {
           <MobileDrawer />
         </Container>
       </header>
+      {
+        isFormVisible && (
+          <div sx={styles.formOverlay}>
+            <div sx={styles.formContainer}>
+              <button sx={styles.closeButton} onClick={closeForm}>❌</button>
+              <div id="hubspotForm"></div>
+            </div>
+          </div>
+        )
+      }
     </DrawerProvider>
+
   );
 }
 
@@ -112,5 +128,49 @@ const styles = {
         color: 'primary',
       },
     },
+  },
+  formOverlay: {
+    mt: '400px',
+    width: '100%',
+    height: '100%',
+    maxWidth: '100vw',
+    maxHeight: '90vh',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    backdropFilter: 'blur(100px)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1000,
+  },
+  formContainer: {
+    position: 'relative',
+    margin: '20px',
+    padding: '20px',
+    border: '1px solid #ddd',
+    borderRadius: '4px',
+    backgroundColor: '#fff',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.5)',
+    width: '100%',
+    maxWidth: '650px',
+    maxHeight: '90vh',
+    display: 'flex',
+    flexDirection: 'column',
+    overflowY: 'auto',
+  },
+  closeButton: {
+    position: 'fixed',
+    top: '-280px',
+    right: '10px',
+    background: 'none',
+    border: 'none',
+    fontSize: '24px',
+    cursor: 'pointer',
+    color: '#333',
+    zIndex: 1100,
   },
 };
