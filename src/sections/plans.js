b918849eac6b32f7465f73f49c143527fa5d389c
Kelvin Rimui <rimuikelvin4@gmail.com>
2024-08-02 15:38:23 +0300

    refined the sample plans section

--- src/sections/plans.js
+++ src/sections/plans.js
@@ -1,79 +1,137 @@
 /** @jsx jsx */
-import { jsx, Box, Image, Heading, Text, Button, Container } from 'theme-ui';
+import { jsx, Box, Image, Heading, Container } from 'theme-ui';
 import { FaChevronRight } from 'react-icons/fa';
+
+import TwoBedB from '../assets/plans/2bed.jpg';
+import ThreeBedF from '../assets/plans/3bedflat.jpg';
+import ThreeBedB from '../assets/plans/3bedpitched.jpg';
+import FourBedF from '../assets/plans/4bedflat.jpg';
+import FourBedF2 from '../assets/plans/4bedflat2.jpg';
+import FourBedP from '../assets/plans/4bedpitched.jpg';
 
 const cardData = [
     {
         id: 1,
         title: '2 Bedroom Bungalow',
-        image: 'https://via.placeholder.com/300', // Replace with your image URLs
-        description: ['Master bedroom en-suite', 'Spacious lounge', 'Kitchen Island', 'Floor area 84sqm'],
+        image: TwoBedB,
+        description: [
+            'Master bedroom en-suite',
+            'Spacious lounge',
+            'Kitchen Island',
+            'Floor area 84sqm',
+        ],
         price: 'Ksh 3,500,000',
         link: 'https://bbsltd.co.ke/sample-plans/design-a/', // Replace with your links
     },
     {
         id: 2,
         title: '3 Bedroom Bungalow',
-        image: 'https://via.placeholder.com/300',
-        description: ['Spacious lounge', 'Kitchen Pantry', 'Kitchen', 'Accessible flat roof', 'floor area 99sqm'],
+        image: ThreeBedF,
+        description: [
+            'Spacious lounge',
+            'Kitchen Pantry',
+            'Kitchen',
+            'Accessible flat roof',
+            'floor area 99sqm',
+        ],
         price: 'Ksh 4,200,000',
         link: 'https://bbsltd.co.ke/sample-plans/design-b/',
     },
     {
         id: 6,
         title: '3 Bedroom Bungalow',
-        image: 'https://via.placeholder.com/300',
-        description: ['Spacious lounge', 'Kitchen', 'Dining area', 'floor area 105sqm'],
+        image: ThreeBedB,
+        description: [
+            'Spacious lounge',
+            'Kitchen',
+            'Dining area',
+            'floor area 105sqm',
+        ],
         price: 'Ksh 4,400,000',
         link: 'https://bbsltd.co.ke/sample-plans/design-b/',
     },
     {
         id: 4,
         title: '4 Bedroom Mansionette',
-        image: 'https://via.placeholder.com/300',
-        description: ['All bedrooms en-suite', 'Spacious lounge', 'Dining area', 'Kitchen pantry', 'Kitchen', 'Balcony', 'Laundry', 'Walk-in closet', 'floor area 200sqm'],
+        image: FourBedP,
+        description: [
+            'All bedrooms en-suite',
+            'Spacious lounge',
+            'Dining area',
+            'Kitchen pantry',
+            'Kitchen',
+            'Balcony',
+            'Laundry',
+            'Walk-in closet',
+            'floor area 200sqm',
+        ],
         price: 'Ksh 8,400,000',
         link: 'https://bbsltd.co.ke/sample-plans/design-d/',
     },
     {
         id: 5,
         title: '4 Bedroom Mansionette',
-        image: 'https://via.placeholder.com/300',
-        description: ['Master bedroom en-suite', 'Spacious lounge', 'Dining area', 'Kitchen pantry', 'Kitchen', 'Balcony', 'Flatroof', 'Walk-in closet', 'floor area 160sqm'],
+        image: FourBedF,
+        description: [
+            'Master bedroom en-suite',
+            'Spacious lounge',
+            'Dining area',
+            'Kitchen pantry',
+            'Kitchen',
+            'Balcony',
+            'Flatroof',
+            'Walk-in closet',
+            'floor area 160sqm',
+        ],
         price: 'Ksh 6,700,000',
         link: 'https://bbsltd.co.ke/sample-plans/design-f/',
     },
     {
         id: 3,
         title: '4 Bedroom Mansionette',
-        image: 'https://via.placeholder.com/300',
-        description: ['All bedrooms en-suite', 'Spacious lounge', 'Dining area', 'Kitchen pantry', 'Kitchen', 'Balcony', 'Walk-in closet', 'floor area 160sqm'],
+        image: FourBedF2,
+        description: [
+            'All bedrooms en-suite',
+            'Spacious lounge',
+            'Dining area',
+            'Kitchen pantry',
+            'Kitchen',
+            'Balcony',
+            'Walk-in closet',
+            'floor area 160sqm',
+        ],
         price: 'Ksh 6,800,000',
         link: 'https://bbsltd.co.ke/sample-plans/design-e/',
     },
-
 ];
 
 const CardSection = () => {
     return (
         <Container sx={styles.container}>
             {cardData.map((card) => (
-                <Box sx={styles.card} key={card.id}>
-                    <Image src={card.image} sx={styles.image} />
-                    <Box sx={styles.content}>
-                        <Heading as="h3" sx={styles.title}>{card.title}</Heading>
-                        <ul sx={styles.description}>
-                            {card.description.map((item, index) => (
-                                <li key={index}>
-                                    <FaChevronRight sx={styles.icon} /> {item}
-                                </li>
-                            ))}
-                        </ul>
-                        <Text sx={styles.price}>{card.price}</Text>
-                        <Button as="a" href={card.link} sx={styles.button}>
-                            Learn More
-                        </Button>
+                <Box
+                    sx={styles.cardWrapper}
+                    key={card.id}
+                    as="a"
+                    href={card.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Box sx={styles.card}>
+                        <Image src={card.image} sx={styles.image} />
+                        <Box sx={styles.content}>
+                            <ul sx={styles.description}>
+                                {card.description.map((item, index) => (
+                                    <li key={index}>
+                                        <FaChevronRight sx={styles.icon} /> {item}
+                                    </li>
+                                ))}
+                            </ul>
+                        </Box>
                     </Box>
+                    <Heading as="h3" sx={styles.title}>
+                        {card.title}
+                    </Heading>
                 </Box>
             ))}
         </Container>
@@ -87,33 +145,64 @@ const styles = {
         gap: 4,
         my: 6,
     },
+    cardWrapper: {
+        textDecoration: 'none',
+        color: 'inherit',
+        mb: 3, // Add margin-bottom to separate the title from the next card
+    },
     card: {
+        position: 'relative',
         boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
         borderRadius: '8px',
         overflow: 'hidden',
         bg: 'white',
-        transition: 'box-shadow 0.3s ease',
-        '&:hover': {
-            boxShadow: '0px 6px 12px rgba(0, 0, 0, 0.15)',
+        transition: 'box-shadow 0.3s ease, transform 0.3s ease',
+        '@media (hover: hover)': {
+            // Only apply hover effects on devices that support hover
+            '&:hover': {
+                boxShadow: '0px 6px 12px rgba(0, 0, 0, 0.15)',
+                transform: 'scale(1.02)',
+                '& > div': {
+                    opacity: 1, // Show description on hover
+                    top: 0,
+                    bottom: 0,
+                },
+            },
         },
     },
     image: {
         width: '100%',
-        height: 'auto',
-        display: 'block',
+        height: '350px', // Set a fixed height for the images
+        objectFit: 'cover', // Ensure the image fits the container without distortion
+        transition: 'transform 0.3s ease, filter 0.3s ease',
     },
     content: {
         padding: 3,
+        position: 'absolute',
+        top: '100%', // Start from below the card
+        left: '0',
+        right: '0',
+        bg: 'rgba(20, 48, 86, 0.8)', // Background color for the overlay
+        opacity: 0, // Hide content initially
+        transition: 'opacity 0.3s ease, top 0.3s ease, bottom 0.3s ease',
+        '@media screen and (max-width: 768px)': {
+            // Make description always visible on mobile
+            opacity: 1,
+            position: 'static',
+        },
     },
     title: {
         fontSize: 3,
         fontWeight: 'bold',
-        mb: 2,
+        mt: 2,
+        textAlign: 'center',
+        color: 'primary',
     },
     description: {
         listStyle: 'none',
         padding: 0,
         mb: 2,
+        color: 'white',
         '& li': {
             display: 'flex',
             alignItems: 'center',
@@ -122,26 +211,7 @@ const styles = {
     },
     icon: {
         marginRight: 2,
-        color: 'primary',
-    },
-    price: {
-        fontSize: 2,
-        color: 'primary',
-        mb: 3,
-        fontWeight: 'bold',
-    },
-    button: {
-        backgroundColor: 'primary',
         color: 'white',
-        textDecoration: 'none',
-        display: 'inline-block',
-        padding: '10px 20px',
-        borderRadius: '4px',
-        fontWeight: 'bold',
-        transition: 'background-color 0.3s ease',
-        '&:hover': {
-            backgroundColor: 'dark',
-        },
     },
 };
 
